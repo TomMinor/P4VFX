d521a7f4f4e6c2f3934279ec2798d86491c691fa
Minor Tom <i7245143@w10139.student.bournemouth.ac.uk>
2015-12-04 17:47:44 +0000

    Patched windows install scripts

--- uninstall.cmd
+++ uninstall.cmd
@@ -1,15 +1,27 @@
 @echo off
 
-set HOME="%HOMEDRIVE%%HOMEPATH%"
+
+
+set HOME=%HOMEDRIVE%%HOMEPATH%
+
+
+echo %HOME%maya\scripts
+
 
 :: Remove P4Python (Maya)
-del "%HOME%\My Documents\maya\scripts\P4.py"
-del "%HOME%\My Documents\maya\scripts\P4API.pyd"
+
+del %HOME%maya\scripts\P4.py
+del %HOME%maya\scripts\P4API.pyd
+
 
 :: Remove Maya plugin
-del "%HOME%\My Documents\maya\plug-ins\P4Maya.py"
-rmdir "%HOME%\My Documents\maya\scripts\Perforce"
+
+
+del %HOME%maya\plug-ins\P4Maya.py
+rmdir /s /q %HOME%maya\scripts\Perforce
+
 
 :: Remove P4Python (Nuke)
-del "%HOME%\.nuke\P4.py"
-del "%HOME%\.nuke\P4API.pyd"
+
+del %HOME%.nuke\P4.py
+del %HOME%.nuke\P4API.pyd
