1531d875b7264d87ef791acc8d7f11c8c09b1974
Tom Minor <i7245143@bournemouth.ac.uk>
2015-12-04 20:18:17 +0000

    Fixed the install_home.cmd script

--- install_home.cmd
+++ install_home.cmd
@@ -7,27 +7,34 @@ set HOME=%HOMEDRIVE%%HOMEPATH%
 set P4CONFIG=.p4config
 set P4CONFIGPATH=%HOME%\%P4CONFIG%
 
+
+cd /d %~dp0
+Set  StartInDirectory= %cd%
+
+set CURRENTDIR=%StartInDirectory%
+
 type NUL > %P4CONFIGPATH%
 
 :: Install P4Python (Maya)
 md "%HOME%\My Documents\maya\scripts"
 
-echo Linking %cd%\P4API\windows\P4.py...
-mklink /J /d "%HOME%\My Documents\maya\scripts\P4.py" "%cd%\P4API\windows\P4.py"
-echo Linking %cd%\P4API\windows\P4API.pyd...
-mklink /J /d "%HOME%\My Documents\maya\scripts\P4API.pyd" "%cd%\P4API\windows\P4API.pyd"
+echo Linking %CURRENTDIR%\P4API\windows\P4.py...
+mklink /J /d "%HOME%\My Documents\maya\scripts\P4.py" "%CURRENTDIR%\P4API\windows\P4.py"
+
+echo Linking %CURRENTDIR%\P4API\windows\P4API.pyd...
+mklink /J /d "%HOME%\My Documents\maya\scripts\P4API.pyd" "%CURRENTDIR%\P4API\windows\P4API.pyd"
 
 :: Install Maya plugin
 md "%HOME%\My Documents\maya\plug-ins"
-echo Linking %cd%\Plugins\P4Maya.py...
-mklink /J /d "%HOME%\My Documents\maya\plug-ins\P4Maya.py" "%cd%\Plugins\P4Maya.py" 
-echo Linking %cd%\Perforce...
-mklink /J /d "%HOME%\My Documents\maya\scripts\Perforce" "%cd%\Perforce"
+echo Linking %CURRENTDIR%\Plugins\P4Maya.py...
+mklink /J /d "%HOME%\My Documents\maya\plug-ins\P4Maya.py" "%CURRENTDIR%\Plugins\P4Maya.py" 
+echo Linking %CURRENTDIR%\Perforce...
+mklink /J /d "%HOME%\My Documents\maya\scripts\Perforce" "%CURRENTDIR%\Perforce"
 
 :: Install P4Python (Nuke)
 ::md "%HOME%/.nuke/"
-::mklink /J /d "%HOME%\.nuke\P4.py" "%cd%\P4API\windows\P4.py"
-::mklink /J /d "%HOME%\.nuke\P4API.pyd" "%cd%\P4API\windows\P4API.pyd"
+::mklink /J /d "%HOME%\.nuke\P4.py" "%CURRENTDIR%\P4API\windows\P4.py"
+::mklink /J /d "%HOME%\.nuke\P4API.pyd" "%CURRENTDIR%\P4API\windows\P4API.pyd"
 
 setx P4CONFIG %P4CONFIGPATH%
 
