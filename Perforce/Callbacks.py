6edad496aed524d863d420abd8bca47f46f5d33d
Minor Tom <i7245143@w10141.student.bournemouth.ac.uk>
2016-03-07 20:10:00 +0000

    Reference callback resolve fix

--- Perforce/Callbacks.py
+++ Perforce/Callbacks.py
@@ -3,11 +3,14 @@ import os
 
 contactrootenv = "CONTACTROOT"
 referenceCallback = None
-
+submitCallback = None
 
 def cleanupCallbacks():
     if referenceCallback:
         api.MCommandMessage.removeCallback(referenceCallback)
+
+    if submitCallback:
+        pass
 
 
 def initCallbacks():
@@ -23,21 +26,28 @@ def initCallbacks():
 def referenceCallbackFunc(inputBool, inputFile, *args):
     api.MScriptUtil.getBool(inputBool)
 
+    print "Reference callback: Checking file {0}".format(os.environ[contactrootenv])
+
     try:
         contactrootpath = os.environ[contactrootenv]
-        print os.environ[contactrootenv]
     except KeyError as e:
         print "Error", e
         api.MScriptUtil.setBool(inputBool, True)
         return
 
     rawpath = inputFile.rawPath()
+    rawname = inputFile.rawName()
     oldpath = rawpath
     if contactrootpath in rawpath:
         rawpath = rawpath.replace(
             contactrootpath, "${0}".format(contactrootenv))
         inputFile.setRawPath(rawpath)
         print "Remapped {0} -> {1}".format(oldpath, rawpath)
+
+    if contactrootenv in rawpath:
+        resolvedName = os.path.join(rawpath.replace("${0}".format(contactrootenv), contactrootpath), rawname)
+        print rawpath, "->", resolvedName
+        inputFile.overrideResolvedFullName(resolvedName)
 
     # print "RAWPATH", inputFile.rawPath()
     # print "RAWFULLNAME", inputFile.rawFullName()
