5291dcf5e02eb5a0b17876854e77316ae00541f4
Tom Minor <tornronim@gmail.com>
2017-05-18 00:29:16 +0100

    added ability to open a specific UI window from the standalone script

--- src/standalone.py
+++ src/standalone.py
@@ -1,16 +1,48 @@
 import unittest
 import logging
 import os
+import sys
 
 from test_perforce import TestingEnvironment, setupEnvironment
 setupEnvironment()
 
 from perforce.Utils import p4Logger
 from perforce.AppInterop import interop
-from perforce.GUI import initMenu
+# from perforce.GUI import initMenu
+from perforce.GUI import PerforceMenu
 
 
-def setup():
+# This is a convenient way of automatically loading up a specific menu entry for testing
+# Clicking on stuff in a menu is tedious
+def displayMenuItem(ui, args):
+    menuEntries = {
+            "checkout.file":                    ui.checkoutFile,
+            "checkout.folder":                  ui.checkoutFolder,
+            "mark.delete":                      ui.deleteFile,
+            "show.changelist":                  ui.queryOpened,
+            "submit.change":                    ui.submitChange,
+            "sync.all":                         ui.syncAllChanged,
+            "sync.all.force":                   ui.syncAll,
+            "depot.history":                    ui.fileRevisions,
+
+            "file.status":                      ui.querySceneStatus,
+
+            "create.asset":                     ui.createAsset,
+            "create.shot":                      ui.createShot,
+            "login.user":                       ui.loginAsUser,
+            "server.info":                      ui.queryServerStatus,
+            "create.workspace":                 ui.createWorkspace,
+            "set.current.workspace":            ui.setCurrentWorkspace,
+            "delete.pending":                   ui.deletePending
+    }
+
+    for arg in args:
+        if arg in menuEntries:
+            menuEntries[arg]()
+        else:
+            p4Logger().warning('%s isn\'t a key in the menu item dict' % arg)
+
+def setup(args):
     cwd = os.path.realpath(os.path.join(os.path.dirname(__file__), '..'))
     os.environ["P4CONFIG"] = os.path.join(cwd, '.p4config')
 
@@ -21,10 +53,16 @@ def setup():
 
     p4Logger().debug( 'Default cwd: %s' % p4.cwd )
 
-    initMenu(p4)
+    ui = PerforceMenu.MainShelf(p4)
 
-    window.show()
+    if args:
+        displayMenuItem(ui, args)
+    else:
+        ui.addMenu()
+        window.show()
+
     app.exec_()
 
 if __name__ == '__main__':
-    setup()
+    args = sys.argv[1:]
+    setup(args)
